6e6ac9772a591dac6376f8276c1806daf67be6d4
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-12 18:22:35 -0700

    Adds fucntionality to series tab for changing and selecting top series, popular and playing now

--- App07-MoviesSeries-TMDB/SeriesView.swift
+++ App07-MoviesSeries-TMDB/SeriesView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SeriesView: View {
     
     @StateObject var media: MediaModel
+    @State var indexSelected: Int = 0
     
     var body: some View {
         GeometryReader { geo in
@@ -22,10 +23,33 @@ struct SeriesView: View {
                             .foregroundColor(Color("Alizarin"))
                             .padding(.top,40)
                     }
+  //****************************************************************
+                    HStack(spacing: 30) {
+                        ForEach(Section.sections) { section in
+                            Button(action: {
+                                indexSelected = section.id
+                                media.seriesList.removeAll()
+                                media.seriesList.append(contentsOf: LoadSeries(list: indexSelected))
+                            }, label: {
+                                VStack {
+                                    Image(systemName: section.id == indexSelected ? section.image : section.imageUnselected)
+                                        .font(.title)
+                                        .frame(height: 40)
+                                    Text(section.name)
+                                        .font(.Akaya(size: 18))
+                                }
+                                .foregroundColor(Color(section.id == indexSelected ? "SourLemon" : "Alizarin"))
+                            })
+                        }
+                    }
+                    .padding(.vertical, 10)
+
+  
+  //*****************************************************************
                     VStack {
                         ScrollView(.horizontal, showsIndicators: false) {
                             HStack {
-                                ForEach(media.seriesNow) { serie in
+                                ForEach(media.seriesList) { serie in
 //                                    NavigationLink(
 //                                        destination: MediaDetailView(media: serie),
 //                                        label: {
@@ -46,6 +70,18 @@ struct SeriesView: View {
             
         }
         
+    }
+    func LoadSeries(list: Int) -> [Video] {
+        switch list {
+        case 0:
+            return media.seriesNow
+        case 1:
+            return media.seriesPopular
+        case 2:
+            return media.seriesTop
+        default:
+            return media.seriesNow
+        }
     }
 }
 
