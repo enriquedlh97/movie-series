e0326d72df46f6f2f76d9497ea76cbdd79e5435b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-14 15:18:29 -0700

    Created TrailerListView

---
+++ App07-MoviesSeries-TMDB/TrailerListView.swift
@@ -0,0 +1,52 @@
+//
+//  TrailerListView.swift
+//  App07-MoviesSeries-TMDB
+//
+//  Created by Enrique Diaz de Leon Hicks on 4/14/21.
+//
+
+import SwiftUI
+
+struct TrailerListView: View {
+    
+    @StateObject var videos: MediaModel
+    var media: Video
+    var isMovie: Bool
+    @State var trailers = [Trailer]()
+    
+    var body: some View {
+        VStack {
+            List(trailers) { trailer in
+                VStack {
+                    Text(trailer.name)
+                        .font(.Akaya(size: 24))
+                        .foregroundColor(Color("BelizeHole"))
+                    Text(trailer.type)
+                        .font(.Akaya(size: 18))
+                        .foregroundColor(Color("Alizarin"))
+                }
+            }
+        }
+        .onAppear {
+            videos.LoadVideos(id: media.id, isMovie: isMovie) { (returnedTrailers) in
+                trailers.removeAll()
+                trailers.append(contentsOf: returnedTrailers)
+            }
+        }
+        .navigationBarTitle(media.title, displayMode: .inline)
+        .navigationBarColor(UIColor(named: "BelizeHole"))
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text(media.title)
+                    .font(.Akaya(size: 24))
+                    .foregroundColor(Color("Alizarin"))
+            }
+        }
+    }
+}
+
+struct TrailerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrailerListView(videos: <#MediaModel#>, media: <#Video#>, isMovie: <#Bool#>)
+    }
+}
