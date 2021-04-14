a06daefebd8421d62e71949e323be2f48b7262cc
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-14 14:57:56 -0700

    Adds struct to hold trailer data gotten from API

---
+++ App07-MoviesSeries-TMDB/Trailer.swift
@@ -0,0 +1,18 @@
+//
+//  Trailer.swift
+//  App07-MoviesSeries-TMDB
+//
+//  Created by Enriqeu Diaz de Leon Hicks on 4/14/21.
+//
+
+import SwiftUI
+
+struct Trailer: Identifiable {
+    
+    var id: String
+    var name: String
+    var key: String
+    var type: String
+    var site: String
+    
+}
