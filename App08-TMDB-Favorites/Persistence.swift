be4924b86903ad0df7d24c0aac966be19141653d
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-27 08:14:54 -0700

    Adds persistence controller

---
+++ App08-TMDB-Favorites/Persistence.swift
@@ -0,0 +1,26 @@
+//
+//  Persistence.swift
+//  App08-TMDB-Favorites
+//
+//  Created by Enrique Diaz de Leon Hicks on 4/27/21.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "FavoritesModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+}
