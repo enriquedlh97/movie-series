415102964cbbc654c8e65f919b4f847def31beeb
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-27 06:25:42 -0700

    Updates coreData

--- Favorites+CoreDataProperties.swift
+++ Favorites+CoreDataProperties.swift
@@ -20,10 +20,24 @@ extension Favorites {
     @NSManaged public var poster_path: String?
     @NSManaged public var release_date: String?
     @NSManaged public var vote_average: Double
-    @NSManaged public var genre_ids: [Int32]?
+    @NSManaged public var genre_ids: [Int32]
     @NSManaged public var overview: String?
     @NSManaged public var title: String?
     @NSManaged public var is_movie: Bool
+    
+    // These variables set the value to " " for when the variable is null
+    var title_wrapepd: String {
+        title ?? " "
+    }
+    var overview_wrapepd: String {
+        overview ?? " "
+    }
+    var poster_path_wrapepd: String {
+        poster_path ?? " "
+    }
+    var release_date_wrapepd: String {
+        release_date ?? " "
+    }
 
 }
 
