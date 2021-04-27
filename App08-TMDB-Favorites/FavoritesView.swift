b13a0a9e5afd30c0c1972981105088ef53e4117c
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-27 08:11:12 -0700

    Updated favoritesView

--- App08-TMDB-Favorites/FavoritesView.swift
+++ App08-TMDB-Favorites/FavoritesView.swift
@@ -2,7 +2,7 @@
 //  FavoritesView.swift
 //  App08-TMDB-Favorites
 //
-//  Created by user189351 on 4/27/21.
+//  Created by Enrique Diaz de Leon Hicks on 4/27/21.
 //
 
 import SwiftUI
@@ -10,9 +10,19 @@ import SwiftUI
 struct FavoritesView: View {
     
     @StateObject var media = MediaModel()
+    @FetchRequest(
+        entity: Favorites.entity(),
+        sortDescriptors: [NSSortDescriptor(key: "release_date", ascending: false)])
+    var favorites: FetchedResults<Favorites>
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                ForEach(favorites) { fav in
+                    Text(fav.title_wrapepd)
+                }
+            }
+        }
     }
 }
 
