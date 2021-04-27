59c069178d995545a878ccac58fb520e721e0bbb
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-27 09:18:04 -0700

    Updates design of favoritesView

---
+++ App08-TMDB-Favorites/FavoriteCellView.swift
@@ -0,0 +1,47 @@
+//
+//  FavoriteCellView.swift
+//  App08-TMDB-Favorites
+//
+//  Created by Enrique Diaz de Leon Hicks on 4/27/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct FavoriteCellView: View {
+    
+    var fav: Favorites
+    
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    KFImage(URL(string: fav.poster_path_wrapepd)!)
+                        .placeholder {
+                            ProgressView()
+                        }
+                        .resizable()
+                        .scaledToFill()
+                        .frame(
+                            width: geo.size.width-40,
+                            height: geo.size.height-120)
+                        .cornerRadius(40)
+                    VStack {
+                        Text(fav.title_wrapepd)
+                            .font(.Akaya(size: 24))
+                            .multilineTextAlignment(.center)
+                        Text("(\(String(fav.release_date_wrapepd))")
+                            .font(.Akaya(size: 24))
+                            .multilineTextAlignment(.center)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct FavoriteCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteCellView(fav: Favorites())
+    }
+}
